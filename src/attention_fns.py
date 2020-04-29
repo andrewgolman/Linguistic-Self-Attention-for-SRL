fix normalization in transformer layer

src/attention_fns.py
@@ -23,10 +23,7 @@ class CopyFromOutput(FunctionDispatcher):
         else:
             attention_to_copy = eval_attention_to_copy
 
-        attention_to_copy -= tf.math.reduce_mean(attention_to_copy, axis=[1, 2], keepdims=True)
-        return tf.linalg.normalize(attention_to_copy, axis=[1, 2])[0]  # todo verify
-
-        # tf.stop_gradient(eval_attention_to_copy)
+        return attention_to_copy
 
 
 class LabelAttention(FunctionDispatcher):

src/train.py
@@ -125,7 +125,7 @@ def main():
 
     val_datasets = [
         dataset.get_dataset(
-            filename, data_config, lookup_ops,
+            [filename], data_config, lookup_ops,
             batch_size=hparams.validation_batch_size, num_epochs=1, shuffle=False
         ) for filename in dev_filenames
     ]
@@ -145,8 +145,8 @@ def main():
 
     lr_schedule_callback = tf.keras.callbacks.LearningRateScheduler(train_utils.learning_rate_scheduler(hparams))
     eval_callbacks = [
-        callbacks.EvalMetricsCallBack(val_dataset, "{}/metrics_log_{}.txt".format(args.save_dir, filename), eval_every=10)
-        for val_dataset, filename in zip(val_datasets, dev_filenames)
+        callbacks.EvalMetricsCallBack(val_dataset, "{}/metrics_log_{}.txt".format(args.save_dir, i), eval_every=5)
+        for i, val_dataset in enumerate(val_datasets)
     ]
     save_callback = callbacks.SaveCallBack(path=args.save_dir, save_every=10)
 

src/transformer_layer.py
@@ -70,15 +70,14 @@ class MultiHeadAttentionWithSpecial(MultiHeadAttention):
             mask = tf.expand_dims(mask, 1)  # Broadcast on head dimension.
             dot = tf.cast(tf.cast(dot, tf.float32) * mask + ((1.0 - mask) * tf.float32.min), dot.dtype)
 
-        attn = tf.cast(tf.nn.softmax(tf.cast(dot, tf.float32)), dot.dtype)
 
         # Replace last heads with special heads. todo AG optimize
         if len(special_attn) > 0:
-            unstacked_attn = tf.unstack(attn, axis=1)  # [BATCH_SIZE, HEADS, SEQ_LEN, SEQ_LEN]
+            unstacked_attn = tf.unstack(dot, axis=1)  # [BATCH_SIZE, HEADS, SEQ_LEN, SEQ_LEN]
             for i, t in enumerate(special_attn):
                 unstacked_attn[-i] = t
         #         tf.stop_gradient(unstacked_attn[-i])
-            attn = tf.stack(unstacked_attn, axis=1)
+            dot = tf.stack(unstacked_attn, axis=1)
 
         if len(special_values) > 0:
             unstacked_values = tf.unstack(values, axis=1)
@@ -86,6 +85,8 @@ class MultiHeadAttentionWithSpecial(MultiHeadAttention):
                 unstacked_values[-i] = t
         #         tf.stop_gradient(unstacked_attn[-i])
             values = tf.stack(unstacked_values, axis=1)
+
+        attn = tf.cast(tf.nn.softmax(tf.cast(dot, tf.float32)), dot.dtype)
 
         drop_attn = onmt_transformer.common.dropout(attn, self.dropout, training=training)
         heads = tf.matmul(drop_attn, values)